16c555bdefc748d87b0d905ff9235075b861d1f5
zsien <quezhiyong@deepin.org>
2023-05-10 13:11:38 +0800

    fcitx5 input methods

--- src/addons/fcitx5proxy/Fcitx5Proxy.cc
+++ src/addons/fcitx5proxy/Fcitx5Proxy.cc
@@ -1,9 +1,26 @@
 #include "Fcitx5Proxy.h"
 
+#include "DBusProvider.h"
+
 DIM_ADDON_FACTORY(Fcitx5Proxy);
 
 Fcitx5Proxy::Fcitx5Proxy(Dim *dim)
-    : InputMethodAddon(dim) {
+    : InputMethodAddon(dim)
+    , dbusProvider_(new DBusProvider(this))
+    , available_(dbusProvider_->available()) {
+    connect(dbusProvider_, &DBusProvider::availabilityChanged, this, [this](bool available) {
+        if (available_ != available) {
+            available_ = available;
+
+            if (available_) {
+                updateInputMethods();
+            } else {
+                inputMethods_.clear();
+            }
+        }
+    });
+
+    updateInputMethods();
 }
 
 Fcitx5Proxy::~Fcitx5Proxy() {
@@ -12,4 +29,20 @@ Fcitx5Proxy::~Fcitx5Proxy() {
 QList<InputMethodEntry> Fcitx5Proxy::getInputMethods() {
     QList<InputMethodEntry> ret;
     return ret;
+}
+
+void Fcitx5Proxy::updateInputMethods() {
+    auto call = dbusProvider_->controller()->AvailableInputMethods();
+    auto watcher = new QDBusPendingCallWatcher(call, this);
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](QDBusPendingCallWatcher *watcher) {
+        QDBusPendingReply<FcitxQtInputMethodEntryList> ims = *watcher;
+        watcher->deleteLater();
+
+        QList<InputMethodEntry> inputMethods;
+        for (auto &im : ims.value()) {
+            inputMethods.append(InputMethodEntry(im.uniqueName(), im.name(), im.nativeName(), im.label(), im.icon()));
+        }
+
+        inputMethods_.swap(inputMethods);
+    });
 }
--- src/addons/fcitx5proxy/Fcitx5Proxy.h
+++ src/addons/fcitx5proxy/Fcitx5Proxy.h
@@ -3,12 +3,22 @@
 
 #include <dimcore/InputMethodAddon.h>
 
+class DBusProvider;
+
 class Fcitx5Proxy : public InputMethodAddon {
 public:
     Fcitx5Proxy(Dim *dim);
     virtual ~Fcitx5Proxy();
 
     QList<InputMethodEntry> getInputMethods() override;
+
+private:
+    DBusProvider *dbusProvider_;
+    bool available_;
+
+    QList<InputMethodEntry> inputMethods_;
+
+    void updateInputMethods();
 };
 
 #endif // !FCITX5PROXY_H
