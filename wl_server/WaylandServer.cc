d79da1144b0ceb6c256f8b6c770384f4a67d8418
zsien <quezhiyong@deepin.org>
2023-07-03 17:29:57 +0800

    feat: wl_server add CallbackWrapper

--- wl_server/WaylandServer.cc
+++ wl_server/WaylandServer.cc
@@ -1,38 +1,15 @@
 #include "WaylandServer.h"
 
+#include "utils.h"
 #include "wayland-input-method-unstable-v1-server-protocol.h"
 #include "wayland-text-input-unstable-v3-server-protocol.h"
 
 #include <wayland-server.h>
 
-#include <iostream>
-#include <stdexcept>
-
 #include <QDebug>
 
-void text_input_v3_bind([[maybe_unused]] struct wl_client *client,
-                        [[maybe_unused]] void *data,
-                        uint32_t version,
-                        uint32_t id)
-{
-    std::cout << "text_input_v3_bind:" << version << id << std::endl;
-}
-
-void input_method_v1_bind([[maybe_unused]] struct wl_client *client,
-                          [[maybe_unused]] void *data,
-                          uint32_t version,
-                          uint32_t id)
-{
-    std::cout << "input_method_v1_bind:" << version << id << std::endl;
-}
-
-void input_method_context_v1_bind([[maybe_unused]] struct wl_client *client,
-                                  [[maybe_unused]] void *data,
-                                  uint32_t version,
-                                  uint32_t id)
-{
-    std::cout << "input_method_context_v1_bind:" << version << id << std::endl;
-}
+#include <iostream>
+#include <stdexcept>
 
 WaylandServer::WaylandServer()
     : display_(wl_display_create())
@@ -42,13 +19,21 @@ WaylandServer::WaylandServer()
         throw std::runtime_error("Unable to add socket to Wayland display");
     }
 
-    wl_global_create(display_, &zwp_text_input_v3_interface, 1, nullptr, &text_input_v3_bind);
-    wl_global_create(display_, &zwp_input_method_v1_interface, 1, nullptr, &input_method_v1_bind);
+    wl_global_create(display_,
+                     &zwp_text_input_v3_interface,
+                     1,
+                     this,
+                     CallbackWrapper<&WaylandServer::textInputV3Bind>::func);
+    wl_global_create(display_,
+                     &zwp_input_method_v1_interface,
+                     1,
+                     this,
+                     CallbackWrapper<&WaylandServer::inputMethodV1Bind>::func);
     wl_global_create(display_,
                      &zwp_input_method_context_v1_interface,
                      1,
-                     nullptr,
-                     &input_method_context_v1_bind);
+                     this,
+                     CallbackWrapper<&WaylandServer::inputMethodContextV1Bind>::func);
 }
 
 WaylandServer::~WaylandServer()
@@ -59,4 +44,25 @@ WaylandServer::~WaylandServer()
 void WaylandServer::run()
 {
     wl_display_run(display_);
+}
+
+void WaylandServer::textInputV3Bind([[maybe_unused]] struct wl_client *client,
+                                    uint32_t version,
+                                    uint32_t id)
+{
+    std::cout << "text_input_v3_bind:" << version << id << std::endl;
+}
+
+void WaylandServer::inputMethodV1Bind([[maybe_unused]] struct wl_client *client,
+                                      uint32_t version,
+                                      uint32_t id)
+{
+    std::cout << "input_method_v1_bind:" << version << id << std::endl;
+}
+
+void WaylandServer::inputMethodContextV1Bind([[maybe_unused]] struct wl_client *client,
+                                             uint32_t version,
+                                             uint32_t id)
+{
+    std::cout << "input_method_context_v1_bind:" << version << id << std::endl;
 }
--- wl_server/WaylandServer.h
+++ wl_server/WaylandServer.h
@@ -1,9 +1,12 @@
 #ifndef WAYLANDSERVER_H
 #define WAYLANDSERVER_H
 
+#include <stdint.h>
+
 struct wl_display;
 
-class WaylandServer {
+class WaylandServer
+{
 public:
     WaylandServer();
     ~WaylandServer();
@@ -12,6 +15,10 @@ public:
 
 private:
     wl_display *display_;
+
+    void textInputV3Bind(struct wl_client *client, uint32_t version, uint32_t id);
+    void inputMethodV1Bind(struct wl_client *client, uint32_t version, uint32_t id);
+    void inputMethodContextV1Bind(struct wl_client *client, uint32_t version, uint32_t id);
 };
 
 #endif // !WAYLANDSERVER_H
