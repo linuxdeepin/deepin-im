93cb94c0d03e22ab74d6c789409a61ebe72d3a4d
zsien <quezhiyong@deepin.org>
2023-07-05 13:21:44 +0800

    feat: add text_input_manager

--- wl_server/WaylandServer.cc
+++ wl_server/WaylandServer.cc
@@ -19,21 +19,29 @@ WaylandServer::WaylandServer()
         throw std::runtime_error("Unable to add socket to Wayland display");
     }
 
-    wl_global_create(display_,
-                     &zwp_text_input_v3_interface,
-                     1,
-                     this,
-                     CallbackWrapper<&WaylandServer::textInputV3Bind>::func);
-    wl_global_create(display_,
-                     &zwp_input_method_v1_interface,
-                     1,
-                     this,
-                     CallbackWrapper<&WaylandServer::inputMethodV1Bind>::func);
-    wl_global_create(display_,
-                     &zwp_input_method_context_v1_interface,
-                     1,
-                     this,
-                     CallbackWrapper<&WaylandServer::inputMethodContextV1Bind>::func);
+    text_input_manager_global_ =
+        wl_global_create(display_,
+                         &zwp_text_input_manager_v3_interface,
+                         1,
+                         this,
+                         CallbackWrapper<&WaylandServer::textInputManagerV3Bind>::func);
+    text_input_global_ = wl_global_create(display_,
+                                          &zwp_text_input_v3_interface,
+                                          1,
+                                          this,
+                                          CallbackWrapper<&WaylandServer::textInputV3Bind>::func);
+    input_method_global_ =
+        wl_global_create(display_,
+                         &zwp_input_method_v1_interface,
+                         1,
+                         this,
+                         CallbackWrapper<&WaylandServer::inputMethodV1Bind>::func);
+    input_method_context_global_ =
+        wl_global_create(display_,
+                         &zwp_input_method_context_v1_interface,
+                         1,
+                         this,
+                         CallbackWrapper<&WaylandServer::inputMethodContextV1Bind>::func);
 }
 
 WaylandServer::~WaylandServer()
@@ -44,6 +52,38 @@ WaylandServer::~WaylandServer()
 void WaylandServer::run()
 {
     wl_display_run(display_);
+}
+
+static const struct zwp_text_input_manager_v3_interface
+    zwp_text_input_manager_v3_interface_implementation = {
+        // todo:
+    };
+
+static void
+zwp_text_input_manager_v3_resource_destroy([[maybe_unused]] struct wl_resource *resource)
+{
+    // struct my_output *client_output = wl_resource_get_user_data(resource);
+
+    // // TODO: Clean up resource
+
+    // remove_to_list(client_output->state->client_outputs, client_output);
+}
+
+void WaylandServer::textInputManagerV3Bind([[maybe_unused]] struct wl_client *client,
+                                           uint32_t version,
+                                           uint32_t id)
+{
+    std::cout << "text_input_manager_v3_bind:" << version << id << std::endl;
+
+    struct wl_resource *resource = wl_resource_create(client,
+                                                      &zwp_text_input_manager_v3_interface,
+                                                      zwp_text_input_v3_interface.version,
+                                                      id);
+
+    wl_resource_set_implementation(resource,
+                                   &zwp_text_input_manager_v3_interface_implementation,
+                                   this,
+                                   &zwp_text_input_manager_v3_resource_destroy);
 }
 
 void WaylandServer::textInputV3Bind([[maybe_unused]] struct wl_client *client,
--- wl_server/WaylandServer.h
+++ wl_server/WaylandServer.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+struct wl_global;
 struct wl_display;
 
 class WaylandServer
@@ -15,7 +16,12 @@ public:
 
 private:
     wl_display *display_;
+    wl_global *text_input_manager_global_;
+    wl_global *text_input_global_;
+    wl_global *input_method_global_;
+    wl_global *input_method_context_global_;
 
+    void textInputManagerV3Bind(struct wl_client *client, uint32_t version, uint32_t id);
     void textInputV3Bind(struct wl_client *client, uint32_t version, uint32_t id);
     void inputMethodV1Bind(struct wl_client *client, uint32_t version, uint32_t id);
     void inputMethodContextV1Bind(struct wl_client *client, uint32_t version, uint32_t id);
