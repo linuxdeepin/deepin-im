7be67e81de092d26d6fef0f1d78c461a292ecec0
dengbo <dengbo@deepin.org>
2023-09-19 11:02:21 +0800

    feat: add log
    add log
    
    Log:

--- src/dim/main.cpp
+++ src/dim/main.cpp
@@ -2,13 +2,20 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "dimcore/Dim.h"
+
+#include <DLog>
+
 #include <QGuiApplication>
 
-#include "dimcore/Dim.h"
+using Dtk::Core::DLogManager;
 
 int main(int argc, char *argv[])
 {
     QGuiApplication a(argc, argv);
+
+    DLogManager::registerJournalAppender();
+    DLogManager::registerConsoleAppender();
 
     org::deepin::dim::Dim dim;
 
