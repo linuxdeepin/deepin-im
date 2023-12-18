af389f1ec9722a3c642310acb01a074f5c676d7a
dengbo <dengbo@deepin.org>
2023-12-18 16:33:38 +0800

    feat: add treeland-foreign-toplevel-manager-v1 protocols
    support add treeland-foreign-toplevel-manager-v1 protocols
    
    Log:

--- src/addons/wlfrontend/WlrAppMonitor.cpp
+++ src/addons/wlfrontend/WlrAppMonitor.cpp
@@ -1,6 +1,7 @@
 #include "WlrAppMonitor.h"
 
 #include "ForeignToplevelManagementV1.h"
+#include "TreelandForeignToplevelManagementV1.h"
 #include "wl/client/ConnectionBase.h"
 
 #include <QDebug>
@@ -16,6 +17,13 @@ WlrAppMonitor::WlrAppMonitor(const std::shared_ptr<wl::client::ConnectionBase> &
             refresh();
         });
     }
+
+    treelandToplevelManager_ = conn->getGlobal<TreelandForeignToplevelManagerV1>();
+    if (treelandToplevelManager_) {
+        treelandToplevelManager_->setRefreshCallback([this]() {
+            treelandRefresh();
+        });
+    }
 }
 
 WlrAppMonitor::~WlrAppMonitor() = default;
@@ -27,6 +35,26 @@ void WlrAppMonitor::refresh()
 
     const auto &list = toplevelManager_->list();
     for (auto &i : list) {
+        if (i->appId().empty()) {
+            continue;
+        }
+
+        state.emplace(i->key(), i->appId());
+        if (i->activated()) {
+            focus = i->key();
+        }
+    }
+
+    emit appUpdated(state, focus);
+}
+
+void WlrAppMonitor::treelandRefresh()
+{
+    std::unordered_map<std::string, std::string> state;
+    std::string focus;
+
+    const auto &treelandToplevelHandlelist = treelandToplevelManager_->list();
+    for (auto &i : treelandToplevelHandlelist) {
         if (i->appId().empty()) {
             continue;
         }
